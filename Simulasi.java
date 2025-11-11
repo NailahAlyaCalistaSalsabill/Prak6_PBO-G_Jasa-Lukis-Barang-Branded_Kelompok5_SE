71ee84bda5472489659ea254eaa683af376fdab6
AndhikaArkananta <arkanantaandhika@gmail.com>
2025-11-11 17:19:37 +0700

    Add Implementasi Class Simulasi (main) dan logika perhitungan

---
+++ Simulasi.java
@@ -0,0 +1,43 @@
+// File: Simulasi.java
+// TUGAS ANGGOTA KE-3: LOGIKA UTAMA DAN PERHITUNGAN
+
+public class Simulasi {
+
+    public static void main(String[] args) {
+        
+        // 1. Inisialisasi JasaLukis (Membuat objek dari Anggota 1)
+        String[] barangTersedia = {"Sepatu", "Tas", "Jaket"};
+        JasaLukis myJasa = new JasaLukis("Lukis Branded Keren", "Jl. Kreatif No. 10", barangTersedia);
+
+        myJasa.tampilkanInfoJasa();
+
+        // 2. Mensimulasikan 2 Pesanan Pelanggan (Membuat objek dari Anggota 2)
+        
+        // Pesanan 1
+        Pemesanan pesanan1 = new Pemesanan(
+            myJasa, // Mengirim objek JasaLukis
+            "Rudi", 
+            "Sepatu Converse", 
+            "Desain Awan", 
+            350000.0
+        );
+        pesanan1.tampilkanDetailPemesanan();
+        
+        // Pesanan 2
+        Pemesanan pesanan2 = new Pemesanan(
+            myJasa, // Mengirim objek JasaLukis
+            "Siska", 
+            "Tas Kulit", 
+            "Desain Bunga", 
+            500000.0
+        );
+        pesanan2.tampilkanDetailPemesanan();
+        
+        // 3. Logika Sederhana Menghitung Total Biaya
+        double totalBiaya = pesanan1.getHarga() + pesanan2.getHarga();
+        
+        System.out.println("\n=== HASIL LOGIKA PENGHITUNGAN TOTAL BIAYA ===");
+        System.out.println("Total Biaya dari 2 Pesanan: Rp" + String.format("%,.0f", totalBiaya));
+        System.out.println("=============================================");
+    }
+}
